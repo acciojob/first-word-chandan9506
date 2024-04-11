9318b7ceabc40618ed11b1b6b674ce8b144949dc
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-11 09:42:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function firstWord(s) {
   // your code here
+	if (s=="") {
+		return s;
+	}
+	else if(s != ""){
+		let str="";
+		for (let t of s) {
+			str += t;
+			if (t == " ") {
+				break;
+			}
+		}
+		return str;
+		
+	}
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+  const s = prompt("Enter String:");
 alert(firstWord(s));
