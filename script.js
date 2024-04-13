4a860fa0dc465f0ee24849bf23b05fd3372fd0c8
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-13 17:00:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 function firstWord(s1) {
   // your code here
 	let s = s1.trim() ;
-	if (s=="") {
-		return s;
-	}
-	else if(s != ""){
-		
-		let str="";
-		
-		for (let t of s) {
-			
-			str += t;
-			if (t == " ") {
-				break;
-			}
-		}
-		return str;
-		
-	}
-	
+	let strArr = s.split(' ');
+	return strArr[0];
 }
 
 // Do not change the code below
 
 const s1 = prompt("Enter String:");
 alert(firstWord(s1));
-
